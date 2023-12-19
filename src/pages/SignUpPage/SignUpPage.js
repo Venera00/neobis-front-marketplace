e3510444893773d1f0e8877098a01eb567a178e8
Venera <veneraomurzakkyzy@gmail.com>
2023-12-20 02:25:28 +0600

    Add sign up page

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -1,8 +1,46 @@
 import React from "react";
+import mainLoginImg from "../../assets/mainLoginImage.png";
+import goBackIcon from "../../assets/goBackIcon.svg";
 import "./SignUpPage.css";
+import { Link } from "react-router-dom";
 
 const SignUpPage = () => {
-  return <div></div>;
+  return (
+    <div className="flex space-between">
+      <div className="img-wrapper">
+        <img src={mainLoginImg} className="login-img" />
+      </div>
+
+      <div className="signup-content">
+        <div className="signup-text__wrapper flex">
+          <div className="signup-text flex">
+            <Link to="/" className="flex">
+              <img src={goBackIcon} />
+              <p>Назад</p>
+            </Link>
+            <p className="signup-title">Регистрация</p>
+          </div>
+        </div>
+
+        <form className="signup-form">
+          <input
+            type="text"
+            name="login"
+            placeholder="Имя пользователя"
+            className="form-input"
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Почта"
+            className="form-input"
+          />
+
+          <button className="signup-btn">Далее</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUpPage;
