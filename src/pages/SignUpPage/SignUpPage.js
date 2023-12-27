bbca03aefdc38ccc5886d54971fcf2103475480c
Venera <veneraomurzakkyzy@gmail.com>
2023-12-28 00:32:03 +0600

    Restructure sign up

--- src/pages/SignUpPage/SignUpPage.js
+++ src/pages/SignUpPage/SignUpPage.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { signup } from "../../api/index";
 import userValidationSchema from "../../Schemas/UserRegistrationInput";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field, ErrorMessage, useFormik } from "formik";
 import mainLoginImg from "../../assets/mainLoginImage.png";
 import goBackIcon from "../../assets/goBackIcon.svg";
 import eye from "../../assets/eye.svg";
@@ -35,26 +35,63 @@ const SignUpPage = () => {
     setShowConfirmPassword(!showConfirmPassword);
   };
 
-  const handleSubmit = async (e) => {
-    try {
-      const userInput = {
-        login: login,
-        email: email,
-        password: password,
-        passwordConfirm: passwordConfirm,
-      };
+  const formik = useFormik({
+    initialValues: {
+      login: "",
+      email: "",
+      password: "",
+      passwordConfirm: "",
+    },
+    validationSchema: userValidationSchema,
 
-      const response = await signup(userInput);
-      navigate("/profile");
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
+    onSubmit: async (values) => {
+      try {
+        const userInput = {
+          login: "",
+          email: "",
+          password: "",
+          passwordConfirm: "",
+        };
 
-    console.log("Login:", login);
-    console.log("Email:", email);
-    console.log("Password:", password);
-  };
+        const response = await signup(userInput);
+        if (response && response.success) {
+          navigate("/profile");
+          console.log(response);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+
+      console.log("Login:", login);
+      console.log("Email:", email);
+      console.log("Password:", password);
+    },
+  });
+
+  const { values, errors, touched, handleBlur, handleChange } = formik;
+
+  // const handleSubmit = async (e) => {
+  //   try {
+  //     const userInput = {
+  //       login: "",
+  //       email: "",
+  //       password: "",
+  //       passwordConfirm: "",
+  //     };
+
+  //     const response = await signup(userInput);
+  //     if (response && response.success) {
+  //       navigate("/profile");
+  //       console.log(response);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+
+  //   console.log("Login:", login);
+  //   console.log("Email:", email);
+  //   console.log("Password:", password);
+  // };
 
   return (
     <div className="flex space-between main-container">
@@ -74,120 +111,108 @@ const SignUpPage = () => {
         </div>
 
         <Formik
-          initialValues={{
-            login: login,
-            email: email,
-            password: password,
-            passwordConfirm: passwordConfirm,
-          }}
-          validationSchema={userValidationSchema}
-          onSubmit={handleSubmit}
+          initialValues={formik.initialValues}
+          validationSchema={formik.validationSchema}
+          onSubmit={formik.handleSubmit}
         >
-          {({ values, errors, touched, handleBlur, handleChange }) => (
-            <Form className="signup-form">
-              {!showPasswordInput && (
-                <div className="form-inputs">
-                  <label>
-                    <Field
-                      type="text"
-                      name="login"
-                      value={values.login}
-                      placeholder="Имя пользователя"
-                      className="form-input"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                    />
-                    <ErrorMessage
-                      name="login"
-                      component="div"
-                      className="error-message"
-                    />
-                  </label>
+          <Form className="signup-form">
+            {!showPasswordInput && (
+              <div className="form-inputs">
+                <label>
+                  <Field
+                    type="text"
+                    name="login"
+                    value={values.login}
+                    placeholder="Имя пользователя"
+                    className="form-input"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  <ErrorMessage
+                    name="login"
+                    component="div"
+                    className="error-message"
+                  />
+                </label>
 
-                  <label>
-                    <Field
-                      type="email"
-                      name="email"
-                      value={values.email}
-                      placeholder="Почта"
-                      className="form-input"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
+                <label>
+                  <Field
+                    type="email"
+                    name="email"
+                    value={values.email}
+                    placeholder="Почта"
+                    className="form-input"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="div"
+                    className="error-message"
+                  />
+                </label>
+              </div>
+            )}
+            {showPasswordInput && (
+              <div className="password-inputs">
+                <div className="password-input__wrapper flex space-between">
+                  <Field
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    value={values.password}
+                    onChange={handleChange}
+                    placeholder="Пароль"
+                    className="password-input"
+                  />
+                  <span onClick={handleToggle} className="eye">
+                    <img
+                      src={showPassword ? eye : eyeDisable}
+                      alt="Eye Icon"
+                      className="eye-icon"
                     />
-                    <ErrorMessage
-                      name="email"
-                      component="div"
-                      className="error-message"
-                    />
-                  </label>
+                  </span>
                 </div>
-              )}
+                {errors.password && touched.password && (
+                  <div className="error-message">{errors.password}</div>
+                )}
 
-              {showPasswordInput && (
-                <div className="password-inputs">
-                  <div className="password-input__wrapper flex space-between">
-                    <Field
-                      type={showPassword ? "text" : "password"}
-                      name="password"
-                      value={values.password}
-                      onChange={handleChange}
-                      placeholder="Пароль"
-                      className="password-input"
+                <div className="password-input__wrapper flex space-between">
+                  <Field
+                    type={showConfirmPassword ? "text" : "password"}
+                    name="passwordConfirm"
+                    value={values.passwordConfirm}
+                    onChange={handleChange}
+                    placeholder="Потдтвердите пароль"
+                    className="password-input"
+                  />
+                  <span onClick={handleToggleConfirmPassword} className="eye">
+                    <img
+                      src={showConfirmPassword ? eye : eyeDisable}
+                      alt="Eye Icon"
+                      className="eye-icon"
                     />
-                    <span onClick={handleToggle} className="eye">
-                      <img
-                        src={showPassword ? eye : eyeDisable}
-                        alt="Eye Icon"
-                        className="eye-icon"
-                      />
-                    </span>
-                  </div>
-                  {errors.password && touched.password && (
-                    <div className="error-message">{errors.password}</div>
-                  )}
-
-                  <div className="password-input__wrapper flex space-between">
-                    <Field
-                      type={showConfirmPassword ? "text" : "password"}
-                      name="passwordConfirm"
-                      value={values.passwordConfirm}
-                      onChange={handleChange}
-                      placeholder="Потдтвердите пароль"
-                      className="password-input"
-                    />
-                    <span onClick={handleToggleConfirmPassword} className="eye">
-                      <img
-                        src={showConfirmPassword ? eye : eyeDisable}
-                        alt="Eye Icon"
-                        className="eye-icon"
-                      />
-                    </span>
-                  </div>
-                  {errors.passwordConfirm && touched.passwordConfirm && (
-                    <div className="error-message">
-                      {errors.passwordConfirm}
-                    </div>
-                  )}
+                  </span>
                 </div>
-              )}
-
-              {!showPasswordInput && (
-                <button
-                  onClick={handleNextClick}
-                  type="button"
-                  className="signup-next-btn"
-                >
-                  Далее
-                </button>
-              )}
-
-              {showPasswordInput && (
-                <button type="submit" className={`signup-btn`}>
-                  Далее
-                </button>
-              )}
-            </Form>
-          )}
+                {errors.passwordConfirm && touched.passwordConfirm && (
+                  <div className="error-message">{errors.passwordConfirm}</div>
+                )}
+              </div>
+            )}
+            {!showPasswordInput && (
+              <button
+                onClick={handleNextClick}
+                type="button"
+                className="signup-next-btn"
+              >
+                Далее
+              </button>
+            )}
+            {showPasswordInput && (
+              <button type="submit" className={`signup-btn`}>
+                Далее
+              </button>
+            )}
+          </Form>
         </Formik>
       </div>
     </div>
