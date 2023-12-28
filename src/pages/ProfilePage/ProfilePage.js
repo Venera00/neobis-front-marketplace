4df23b426895fe5dd9856bbcc046066166a318b9
Venera <veneraomurzakkyzy@gmail.com>
2023-12-28 14:53:25 +0600

    Add image upload functionality

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import ProfileNavbar from "../../components/ProfileNavbar/ProfileNavbar";
 import goBackIcon from "../../assets/goBackIcon.svg";
@@ -6,6 +6,32 @@ import "./ProfilePage.css";
 import profileAvatar from "../../assets/profileAvatar.svg";
 
 const ProfilePage = ({ username, email }) => {
+  const [image, setImage] = useState("");
+  const [previewImage, setPreviewImage] = useState(profileAvatar);
+
+  const handleImage = (e) => {
+    const selectedImg = e.target.files[0];
+    setImage(selectedImg);
+    console.log(e.target.files[0]);
+    setPreviewImage(URL.createObjectURL(selectedImg));
+  };
+
+  useEffect(() => {
+    const handleWindowClose = (e) => {
+      if (image === "") {
+        e.preventDefault();
+      }
+    };
+  });
+
+  function handleClick() {
+    const formData = new FormData();
+    formData.append("image", image);
+    // axios.post("url", formData).then((res) => {
+    //   console.log(res);
+    // });
+  }
+
   return (
     <div className="profile-wrapper">
       <ProfileNavbar />
@@ -18,12 +44,16 @@ const ProfilePage = ({ username, email }) => {
           <p className="signup-title">Профиль</p>
         </div>
 
-        <Link className="choose-photo flex">
-          <img src={profileAvatar} />
-          <p>Выбрать фотографию</p>
-        </Link>
-
         <form className="profile-form flex">
+          <div className="choose-photo flex">
+            <img
+              src={previewImage}
+              className="profile-img"
+              alt="Profile image"
+            />
+            <input type="file" onChange={handleImage} id="img-input" />
+            <label htmlFor="img-input">Выберите фотографию</label>
+          </div>
           <input
             type="text"
             placeholder="Имя"
