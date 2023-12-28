a8f20b5ebe4a809ec851d9409ededd51d91284ab
Venera <veneraomurzakkyzy@gmail.com>
2023-12-29 03:55:06 +0600

    Add main page and do nav of page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import SignUpPage from "./pages/SignUpPage/SignUpPage";
 import ProfilePage from "./pages/ProfilePage/ProfilePage";
+import MainPage from "./pages/MainPage/MainPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/main" element={<MainPage />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,27 @@
+import React from "react";
+import mobiMarketIcon from "../../assets/mobiMarcetIcon.svg";
+import profileAvatar from "../../assets/profileAvatar.svg";
+import "./MainPage.css";
+
+const MainPage = (username) => {
+  return (
+    <div>
+      <div className="main-nav flex space-between">
+        <div className="logo-wrapper flex">
+          <img src={mobiMarketIcon} alt="Mobimarket icon" />
+          <p className="icon-title">MOBI MARKET</p>
+        </div>
+        <div className="nav-user__info flex">
+          <button className="nav-btn">Подать обьявление</button>
+          <div className="username-wrapper flex">
+            <p className="username-title">Venera</p>
+            <img src={profileAvatar} alt="Profile avatar" />
+          </div>
+        </div>
+      </div>
+      <div className="cards-container"></div>
+    </div>
+  );
+};
+
+export default MainPage;
